62ff219e7a572c4f63cee0fadddaf2020e586dac
zhudg06@163.com <zhudg06@163.com>
2018-02-25 10:11:42 +0800

    learn exception

--- src/main.py
+++ src/main.py
@@ -4,22 +4,29 @@ Created on 2018年2月18日
 @author: Administrator
 '''
 # -*- coding: utf-8 -*-
+class FormatError(Exception):
+    logfile = 'formaterror.txt'
+    def __init__(self, line, file):
+        self.line = line
+        self.file = file
+    
+    def logerror(self):
+        log = open(self.logfile, 'a')
+        print('Error at', self.file, self.line, file = log)
+    
+def parser():
+    raise FormatError(42, 'spam.txt2')
 
-from test.add import Adder, ListAdder, DictAdder
-from test.mylist import MyList
 
+def main():
+    try:
+        parser()
+    except FormatError as exc:
+        exc.logerror()
+    print('ok')
 
 if __name__ == '__main__':
-    x = MyList('spam')
-    print(x)
-    print(x + [2])
-    print(x * 2)
-    print(x[2])
-    print(x[:-1])
-    print(x + ['eggs'])
-    print(x.append('xxxxx'))
-    x.sort()
-    print(x)
+    main()
     
     
     
